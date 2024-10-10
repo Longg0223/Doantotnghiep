a05e7902eaf720159455b1abdf6abe35dac6f3ff
Longg0223 <102234369+Longg0223@users.noreply.github.com>
2024-10-10 17:41:55 +0700

    Do an tot nghiep

---
+++ DaAnhVan/TECH/TECH/Controllers/Components/RelatedProductsComponent.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using TECH.Areas.Admin.Models;
+using TECH.Service;
+
+namespace TECH.Controllers.Components
+
+{
+    [ViewComponent(Name = "RelatedProductsComponent")]
+    public class RelatedProductsComponent : ViewComponent
+    {
+        private readonly IProductsService _productService;
+        private readonly IProductsImagesService _productsImagesService;
+        public RelatedProductsComponent(IProductsService productService, IProductsImagesService productsImagesService)
+        {
+            _productService = productService;
+            _productsImagesService = productsImagesService;
+    }
+
+        public async Task<IViewComponentResult> InvokeAsync(int? categoryId,int productId)
+        {
+            var model = new List<ProductModelView>();
+            var lstdata = new List<ProductModelView>();
+            if (categoryId.HasValue && categoryId.Value > 0)
+            {
+                var productRelate = _productService.GetProductReLated(categoryId.Value, productId);
+                if (productRelate != null && productRelate.Count > 0)
+                {
+                    lstdata = productRelate.ToList();
+                }
+            }
+            return View(lstdata);
+        }
+    }
+}
---
+++ DaAnhVan/TECH/TECH/Data/DatabaseEntity/DataBaseEntityContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TECH.Data.DatabaseEntity
+{
+    public class DataBaseEntityContext : DbContext
+    {
+        public DataBaseEntityContext(DbContextOptions<DataBaseEntityContext> options) : base(options) { }
+
+        public DbSet<Users> users { set; get; }
+        public DbSet<Contracts> contacts { set; get; }
+        public DbSet<Category> categories { set; get; }
+        public DbSet<Products> products { set; get; }
+        public DbSet<ProductQuantity> product_quantity { set; get; }
+        public DbSet<ProductImages> products_images { set; get; }
+        public DbSet<Images> images { set; get; }
+        public DbSet<Posts> posts { set; get; }
+        public DbSet<Orders> orders { set; get; }
+        public DbSet<Reviews> reviews { set; get; }
+        public DbSet<OrdersDetails> order_details { set; get; }
+        public DbSet<Carts> carts { set; get; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);            
+        }
+    }
+}
